d0ac56a457d7d18664403eb121b5fdb64e8ec24a
Hitesh Tare <hiteshtare@gmail.com>
2021-07-09 13:38:22 +0530

    Implemented utterance updation dynamically

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 // Import node modules
-import { createReadStream, readFileSync } from "fs";
+import { createReadStream, readFile, readFileSync } from "fs";
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 
@@ -10,6 +10,8 @@ import { AudioConfig, SpeechSynthesizer } from "microsoft-cognitiveservices-spee
 // Import custom modules
 import { APP_CONFIG, GLOBAL_VARIABLES } from './config/index';
 import { getLoggerLevel, setLoggerLevel } from './utils/common.util';
+import { XmlDocument } from "xmldoc";
+import { parseString } from "xml2js";
 
 // Import bot configuration/variables from .env file in root folder
 const ENV_FILE = path.join(__dirname, '.env');
@@ -26,37 +28,60 @@ APP_CONFIG.CUSTOM_SPEECH_Region = process.env.CUSTOM_SPEECH_REGION || '';
 _logger.info(`CUSTOM_SPEECH_Subscription_Key: ${APP_CONFIG.CUSTOM_SPEECH_Subscription_Key}`);
 _logger.info(`CUSTOM_SPEECH_Region: ${APP_CONFIG.CUSTOM_SPEECH_Region}`);
 
-// readFileByLine('src/assets/doc/data_prep.txt');
+readFileByLine('src/assets/doc/data_prep.txt');
 
-// GLOBAL_VARIABLES.arrLine.forEach((utterance: string, index: number) => {
-//   _logger.debug(`${index}: ${utterance}`);
+GLOBAL_VARIABLES.arrLine.forEach((utterance: string, index: number) => {
+  _logger.debug(`${index}: ${utterance}`);
 
-//   // createAudioByLine(utterance);
-// });
+  createAudioByLine(utterance);
+});
 
-createAudioByLine();
+// createAudioByLine();
 
 ////////////////////////////////////////////////////////////////////////////////
-function createAudioByLine() {
+function createAudioByLine(utterance: string) {
   const speechConfig = sdk.SpeechConfig.fromSubscription(APP_CONFIG.CUSTOM_SPEECH_Subscription_Key, APP_CONFIG.CUSTOM_SPEECH_Region);
-  const audioConfig = AudioConfig.fromAudioFileOutput(`src/assets/audio/ssml.wav`);
+  const audioConfig = AudioConfig.fromAudioFileOutput(`src/assets/audio/${utterance}.wav`);
 
-  const ssml = xmlToString(`src/assets/sample/ssml.xml`);
+  // read XML file
+  readFile(`src/assets/sample/ssml.xml`, "utf-8", (err, data) => {
+    if (err) {
+      throw err;
+    }
 
-  const synthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
-  synthesizer.speakSsmlAsync(
-    ssml,
-    result => {
-      synthesizer.close();
-      if (result) {
-        // return result as stream
-        return createReadStream(`src/assets/audio/ssml.wav`);
+    // convert XML data to JSON object
+    parseString(data, (err, result) => {
+      if (err) {
+        throw err;
       }
-    },
-    error => {
-      console.log(error);
-      synthesizer.close();
+
+      result.speak.voice[0]['_'] = `${utterance}`;
+
+      //Replacing voice text with utterance
+      _logger.debug(result.speak.voice[0]['_']);
+
+      _logger.info('New XML');
+      _logger.debug(JSON.stringify(result, null, 4));
+
+
+      const synthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+      synthesizer.speakSsmlAsync(
+        result,
+        result => {
+          synthesizer.close();
+          if (result) {
+            // return result as stream
+            return createReadStream(`src/assets/audio/${utterance}.wav`);
+          }
+        },
+        error => {
+          console.log(error);
+          synthesizer.close();
+        });
     });
+  });
+
+
 }
 
 function readFileByLine(path: string) {
